ea5aa0f8366d28f1bcbbb679f8bbac15914f9293
Yash <yashgovind123@gmail.com>
2025-02-16 01:25:49 +0530

    feat: add new travel destinations including Pyramids of Giza, Eiffel Tower, and more

--- data.js
+++ data.js
@@ -35,4 +35,112 @@ export default [
         dates: "01 Oct, 2021 - 18 Nov, 2021",
         text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
     },
-]
+    {
+        id: 4,
+        img: {
+            src: "https://scrimba.com/links/travel-journal-egypt-image-url",
+            alt: "Pyramids of Giza"
+        },
+        title: "Pyramids of Giza",
+        country: "Egypt",
+        googleMapsLink: "https://maps.app.goo.gl/XcNcXWQjv8XQaKjD9",
+        dates: "15 Mar, 2022 - 28 Mar, 2022",
+        text: "The Pyramids of Giza are ancient monuments in Egypt and one of the Seven Wonders of the Ancient World. These pyramids are a lasting symbol of Egypt's rich history."
+    },
+    {
+        id: 5,
+        img: {
+            src: "https://scrimba.com/links/travel-journal-france-image-url",
+            alt: "Eiffel Tower"
+        },
+        title: "Eiffel Tower",
+        country: "France",
+        googleMapsLink: "https://maps.app.goo.gl/8b2FZftLCyK5XjZ67",
+        dates: "10 Apr, 2022 - 22 Apr, 2022",
+        text: "The Eiffel Tower is a wrought-iron lattice tower in Paris, France. It is one of the most recognizable structures in the world and a symbol of French culture."
+    },
+    {
+        id: 6,
+        img: {
+            src: "https://scrimba.com/links/travel-journal-usa-image-url",
+            alt: "Grand Canyon"
+        },
+        title: "Grand Canyon",
+        country: "United States",
+        googleMapsLink: "https://maps.app.goo.gl/Z1VbU5GZb5UpK9VK6",
+        dates: "05 Jul, 2022 - 15 Jul, 2022",
+        text: "The Grand Canyon is a steep-sided canyon carved by the Colorado River in Arizona, USA. It is considered one of the natural wonders of the world."
+    },
+    {
+        id: 7,
+        img: {
+            src: "https://scrimba.com/links/travel-journal-brazil-image-url",
+            alt: "Christ the Redeemer"
+        },
+        title: "Christ the Redeemer",
+        country: "Brazil",
+        googleMapsLink: "https://maps.app.goo.gl/B3cG8F2F5kqX9RX48",
+        dates: "20 Aug, 2022 - 30 Aug, 2022",
+        text: "Christ the Redeemer is an iconic statue of Jesus Christ in Rio de Janeiro, Brazil. It stands atop Mount Corcovado and overlooks the city."
+    },
+    {
+        id: 8,
+        img: {
+            src: "https://scrimba.com/links/travel-journal-italy-image-url",
+            alt: "Colosseum"
+        },
+        title: "Colosseum",
+        country: "Italy",
+        googleMapsLink: "https://maps.app.goo.gl/Xb8FjWkqR1Z9L6P58",
+        dates: "03 Sep, 2022 - 13 Sep, 2022",
+        text: "The Colosseum is an ancient amphitheater in Rome, Italy. It is the largest amphitheater ever built and is a symbol of the power of the Roman Empire."
+    },
+    {
+        id: 9,
+        img: {
+            src: "https://scrimba.com/links/travel-journal-uk-image-url",
+            alt: "Big Ben"
+        },
+        title: "Big Ben",
+        country: "United Kingdom",
+        googleMapsLink: "https://maps.app.goo.gl/Y8hJ8W5Gc2Z9L7K47",
+        dates: "12 Oct, 2022 - 25 Oct, 2022",
+        text: "Big Ben is the nickname for the Great Bell of the clock at the north end of the Palace of Westminster in London, United Kingdom. It is an iconic symbol of London."
+    },
+    {
+        id: 10,
+        img: {
+            src: "https://scrimba.com/links/travel-journal-india-image-url",
+            alt: "Taj Mahal"
+        },
+        title: "Taj Mahal",
+        country: "India",
+        googleMapsLink: "https://maps.app.goo.gl/Z1GvB5GZb5UpK8VK7",
+        dates: "10 Nov, 2022 - 22 Nov, 2022",
+        text: "The Taj Mahal is an ivory-white marble mausoleum on the right bank of the river Yamuna in Agra, India. It was commissioned in 1632 by Mughal emperor Shah Jahan."
+    },
+    {
+        id: 11,
+        img: {
+            src: "https://scrimba.com/links/travel-journal-china-image-url",
+            alt: "Great Wall of China"
+        },
+        title: "Great Wall of China",
+        country: "China",
+        googleMapsLink: "https://maps.app.goo.gl/X1FjW5GZb5UpK9VK8",
+        dates: "05 Dec, 2022 - 18 Dec, 2022",
+        text: "The Great Wall of China is a series of fortifications built across the historical northern borders of China. It is one of the most impressive architectural feats in history."
+    },
+    {
+        id: 12,
+        img: {
+            src: "https://scrimba.com/links/travel-journal-greece-image-url",
+            alt: "Santorini"
+        },
+        title: "Santorini",
+        country: "Greece",
+        googleMapsLink: "https://maps.app.goo.gl/Y2J8F5Gc2Z9L7K59",
+        dates: "10 Jan, 2023 - 20 Jan, 2023",
+        text: "Santorini is a volcanic island in the Cyclades group of the Greek islands. It is famous for its stunning sunsets, white-washed buildings, and blue-domed churches."
+    }
+];
