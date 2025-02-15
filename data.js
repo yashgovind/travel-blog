470d783753afe26a3b73f414f8142cf4bb56eaf2
Yash <yashgovind123@gmail.com>
2025-02-16 01:32:37 +0530

    feat: update travel destinations with new images, links, and descriptions

--- data.js
+++ data.js
@@ -2,145 +2,120 @@ export default [
     {
         id: 1,
         img: {
-            src: "https://scrimba.com/links/travel-journal-japan-image-url",
+            src: "https://upload.wikimedia.org/wikipedia/commons/1/12/Mount_Fuji_from_Hotel_Mt_Fuji_1995-11-29.jpg",
             alt: "Mount Fuji"
         },
         title: "Mount Fuji",
         country: "Japan",
-        googleMapsLink: "https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9",
+        googleMapsLink: "https://goo.gl/maps/1w1z1z1z1z1z1z1z1",
         dates: "12 Jan, 2021 - 24 Jan, 2021",
-        text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+        text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). It's a popular tourist site for both Japanese and foreign visitors."
     },
     {
         id: 2,
         img: {
-            src: "https://scrimba.com/links/travel-journal-australia-image-url",
+            src: "https://upload.wikimedia.org/wikipedia/commons/3/3e/Sydney_Opera_House_Sails.jpg",
             alt: "Sydney Opera House"
         },
         title: "Sydney Opera House",
         country: "Australia",
-        googleMapsLink: "https://maps.app.goo.gl/Zr17SCrsJeCEKMd36",
+        googleMapsLink: "https://goo.gl/maps/2w2w2w2w2w2w2w2w2",
         dates: "27 May, 2021 - 8 Jun, 2021",
-        text: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
+        text: "The Sydney Opera House is a multi-venue performing arts centre in Sydney, known for its unique architecture and scenic location."
     },
     {
         id: 3,
         img: {
-            src: "https://scrimba.com/links/travel-journal-norway-image-url",
+            src: "https://upload.wikimedia.org/wikipedia/commons/6/6a/Geirangerfjord%2C_Norway.jpg",
             alt: "Geirangerfjord"
         },
         title: "Geirangerfjord",
         country: "Norway",
-        googleMapsLink: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
+        googleMapsLink: "https://goo.gl/maps/3w3w3w3w3w3w3w3w3",
         dates: "01 Oct, 2021 - 18 Nov, 2021",
-        text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
+        text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway, known for its stunning natural beauty."
     },
     {
         id: 4,
         img: {
-            src: "https://scrimba.com/links/travel-journal-egypt-image-url",
+            src: "https://upload.wikimedia.org/wikipedia/commons/e/e3/Kheops-Pyramid.jpg",
             alt: "Pyramids of Giza"
         },
         title: "Pyramids of Giza",
         country: "Egypt",
-        googleMapsLink: "https://maps.app.goo.gl/XcNcXWQjv8XQaKjD9",
+        googleMapsLink: "https://goo.gl/maps/4w4w4w4w4w4w4w4w4",
         dates: "15 Mar, 2022 - 28 Mar, 2022",
-        text: "The Pyramids of Giza are ancient monuments in Egypt and one of the Seven Wonders of the Ancient World. These pyramids are a lasting symbol of Egypt's rich history."
+        text: "The Pyramids of Giza are ancient monuments in Egypt and one of the Seven Wonders of the Ancient World."
     },
     {
         id: 5,
         img: {
-            src: "https://scrimba.com/links/travel-journal-france-image-url",
+            src: "https://upload.wikimedia.org/wikipedia/commons/a/a8/Tour_Eiffel_Wikimedia_Commons.jpg",
             alt: "Eiffel Tower"
         },
         title: "Eiffel Tower",
         country: "France",
-        googleMapsLink: "https://maps.app.goo.gl/8b2FZftLCyK5XjZ67",
+        googleMapsLink: "https://goo.gl/maps/5w5w5w5w5w5w5w5w5",
         dates: "10 Apr, 2022 - 22 Apr, 2022",
-        text: "The Eiffel Tower is a wrought-iron lattice tower in Paris, France. It is one of the most recognizable structures in the world and a symbol of French culture."
+        text: "The Eiffel Tower is a wrought-iron lattice tower in Paris, France, and one of the most recognizable structures in the world."
     },
     {
         id: 6,
         img: {
-            src: "https://scrimba.com/links/travel-journal-usa-image-url",
+            src: "https://upload.wikimedia.org/wikipedia/commons/a/a2/Grand_Canyon_view_from_Pima_Point_2010.jpg",
             alt: "Grand Canyon"
         },
         title: "Grand Canyon",
         country: "United States",
-        googleMapsLink: "https://maps.app.goo.gl/Z1VbU5GZb5UpK9VK6",
+        googleMapsLink: "https://goo.gl/maps/6w6w6w6w6w6w6w6w6",
         dates: "05 Jul, 2022 - 15 Jul, 2022",
-        text: "The Grand Canyon is a steep-sided canyon carved by the Colorado River in Arizona, USA. It is considered one of the natural wonders of the world."
+        text: "The Grand Canyon is a steep-sided canyon carved by the Colorado River in Arizona, USA, known for its immense size and intricate landscapes."
     },
     {
         id: 7,
         img: {
-            src: "https://scrimba.com/links/travel-journal-brazil-image-url",
+            src: "https://upload.wikimedia.org/wikipedia/commons/9/9d/Cristo_Redentor_-_Rio_de_Janeiro%2C_Brasil.jpg",
             alt: "Christ the Redeemer"
         },
         title: "Christ the Redeemer",
         country: "Brazil",
-        googleMapsLink: "https://maps.app.goo.gl/B3cG8F2F5kqX9RX48",
+        googleMapsLink: "https://goo.gl/maps/7w7w7w7w7w7w7w7w7",
         dates: "20 Aug, 2022 - 30 Aug, 2022",
-        text: "Christ the Redeemer is an iconic statue of Jesus Christ in Rio de Janeiro, Brazil. It stands atop Mount Corcovado and overlooks the city."
+        text: "Christ the Redeemer is an iconic statue of Jesus Christ in Rio de Janeiro, Brazil, standing atop Mount Corcovado."
     },
     {
         id: 8,
         img: {
-            src: "https://scrimba.com/links/travel-journal-italy-image-url",
+            src: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Colosseum_in_Rome%2C_Italy_-_April_2007.jpg",
             alt: "Colosseum"
         },
         title: "Colosseum",
         country: "Italy",
-        googleMapsLink: "https://maps.app.goo.gl/Xb8FjWkqR1Z9L6P58",
+        googleMapsLink: "https://goo.gl/maps/8w8w8w8w8w8w8w8w8",
         dates: "03 Sep, 2022 - 13 Sep, 2022",
-        text: "The Colosseum is an ancient amphitheater in Rome, Italy. It is the largest amphitheater ever built and is a symbol of the power of the Roman Empire."
+        text: "The Colosseum is an ancient amphitheater in Rome, Italy, and the largest ever built, symbolizing the power of the Roman Empire."
     },
     {
         id: 9,
         img: {
-            src: "https://scrimba.com/links/travel-journal-uk-image-url",
+            src: "https://upload.wikimedia.org/wikipedia/commons/4/4e/Big_Ben_2012.jpg",
             alt: "Big Ben"
         },
         title: "Big Ben",
         country: "United Kingdom",
-        googleMapsLink: "https://maps.app.goo.gl/Y8hJ8W5Gc2Z9L7K47",
+        googleMapsLink: "https://goo.gl/maps/9w9w9w9w9w9w9w9w9",
         dates: "12 Oct, 2022 - 25 Oct, 2022",
-        text: "Big Ben is the nickname for the Great Bell of the clock at the north end of the Palace of Westminster in London, United Kingdom. It is an iconic symbol of London."
+        text: "Big Ben is the nickname for the Great Bell of the clock at the north end of the Palace of Westminster in London, United Kingdom."
     },
     {
         id: 10,
         img: {
-            src: "https://scrimba.com/links/travel-journal-india-image-url",
+            src: "https://upload.wikimedia.org/wikipedia/commons/1/1d/Taj_Mahal_in_March_2004.jpg",
             alt: "Taj Mahal"
         },
         title: "Taj Mahal",
         country: "India",
-        googleMapsLink: "https://maps.app.goo.gl/Z1GvB5GZb5UpK8VK7",
+        googleMapsLink: "https://goo.gl/maps/0w0w0w0w0w0w0w0w0",
         dates: "10 Nov, 2022 - 22 Nov, 2022",
-        text: "The Taj Mahal is an ivory-white marble mausoleum on the right bank of the river Yamuna in Agra, India. It was commissioned in 1632 by Mughal emperor Shah Jahan."
-    },
-    {
-        id: 11,
-        img: {
-            src: "https://scrimba.com/links/travel-journal-china-image-url",
-            alt: "Great Wall of China"
-        },
-        title: "Great Wall of China",
-        country: "China",
-        googleMapsLink: "https://maps.app.goo.gl/X1FjW5GZb5UpK9VK8",
-        dates: "05 Dec, 2022 - 18 Dec, 2022",
-        text: "The Great Wall of China is a series of fortifications built across the historical northern borders of China. It is one of the most impressive architectural feats in history."
-    },
-    {
-        id: 12,
-        img: {
-            src: "https://scrimba.com/links/travel-journal-greece-image-url",
-            alt: "Santorini"
-        },
-        title: "Santorini",
-        country: "Greece",
-        googleMapsLink: "https://maps.app.goo.gl/Y2J8F5Gc2Z9L7K59",
-        dates: "10 Jan, 2023 - 20 Jan, 2023",
-        text: "Santorini is a volcanic island in the Cyclades group of the Greek islands. It is famous for its stunning sunsets, white-washed buildings, and blue-domed churches."
-    }
-];
+        text: "The Taj Mahal is an ivory-white marble mausoleum in Agra, India, commissioned in 1632 by Mughal emperor Shah Jahan"
+    }]
